5b0d88f0df384160eae23d553de79781b4979a64
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-09-08 00:20:43 +0600

    added practice file

---
+++ practice.js
@@ -0,0 +1,20 @@
+// ১. setTimeout এর ভিতরে একটা কিছু কনসোল লগ করবে যেটা সাড়ে তিন সেকেন্ড পরে আউটপুট দেখাবে
+
+// console.log("This is my first 1 message");
+// console.log("This is my second 2 message");
+// console.log("This is my third 3 message");
+// setTimeout(doSomething, 3000);
+// console.log("This is my fifth 5 message");
+
+// function doSomething() {
+//     console.log("This is my forth 4 message")
+// }
+
+
+console.log("This is my first 1 message");
+console.log("This is my second 2 message");
+console.log("This is my third 3 message");
+setTimeout(() => console.log("This is my forth 4 message"), 3000);
+console.log("This is my fifth 5 message");
+
+// ২. prompt দিয়ে ইউজারের কাছ থেকে একটা সংখ্যা নাও। সেটার সাথে ২০০যোগ করো। যোগ করতে গেলে parse করা লাগলে সেটা করো। তারপর সেই রেজাল্টকে alert দিয়ে আউটপুট দেখাও 
