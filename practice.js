22f0fa0cfb08fe3ed7ee03d115c4745c8ae96714
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-09-08 01:18:36 +0600

    2nd problem done

--- practice.js
+++ practice.js
@@ -11,10 +11,22 @@
 // }
 
 
-console.log("This is my first 1 message");
-console.log("This is my second 2 message");
-console.log("This is my third 3 message");
-setTimeout(() => console.log("This is my forth 4 message"), 3000);
-console.log("This is my fifth 5 message");
+
+// console.log("This is my first 1 message");
+// console.log("This is my second 2 message");
+// console.log("This is my third 3 message");
+// setTimeout(() => console.log("This is my forth 4 message"), 3000);
+// console.log("This is my fifth 5 message");
+
 
 // ২. prompt দিয়ে ইউজারের কাছ থেকে একটা সংখ্যা নাও। সেটার সাথে ২০০যোগ করো। যোগ করতে গেলে parse করা লাগলে সেটা করো। তারপর সেই রেজাল্টকে alert দিয়ে আউটপুট দেখাও 
+
+const showPrompt = () => {
+    const stringNumber = prompt('Give me a Number');
+    const convertInt = parseFloat(stringNumber);
+    const total = convertInt + 200;
+    console.log(total);
+    alert(`Total number is : ${total}`);
+
+
+}
