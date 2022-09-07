d7e27bc8a35825d7b0d1f45d6dd43f69d1873918
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-09-08 01:39:41 +0600

    solve 3

--- practice.js
+++ practice.js
@@ -19,14 +19,28 @@
 // console.log("This is my fifth 5 message");
 
 
-// ২. prompt দিয়ে ইউজারের কাছ থেকে একটা সংখ্যা নাও। সেটার সাথে ২০০যোগ করো। যোগ করতে গেলে parse করা লাগলে সেটা করো। তারপর সেই রেজাল্টকে alert দিয়ে আউটপুট দেখাও 
+// ২. prompt দিয়ে ইউজারের কাছ থেকে একটা সংখ্যা নাও। সেটার সাথে ২০০যোগ করো। যোগ করতে গেলে parse করা লাগলে সেটা করো। তারপর সেই রেজাল্টকে alert দিয়ে আউটপুট দেখাও
 
-const showPrompt = () => {
-    const stringNumber = prompt('Give me a Number');
-    const convertInt = parseFloat(stringNumber);
-    const total = convertInt + 200;
-    console.log(total);
-    alert(`Total number is : ${total}`);
+// const showPrompt = () => {
+//     const stringNumber = prompt('Give me a Number');
+//     const convertInt = parseFloat(stringNumber);
+//     const total = convertInt + 200;
+//     console.log(total);
+//     alert(`Total number is : ${total}`);
 
+
+// }
+
+// ৩. ব্রাউজারে একটা confirm করে জিজ্ঞেস করো তুমি কি তোমার ওয়েবসাইট এর লোকেশন দেখতে চাও। যদি বলে দেখতে চাই তাহলে সেই ওয়েবসাইট এর লোকেশন এর href জিনিসটা কনসোল লগ করে দেখাও 
+
+
+const askQuestion = () => {
+    const decision = confirm('are you see your website location?');
+
+    if (decision === true) {
+        console.log(location.href);
+    } else {
+        console.log('Ok bye')
+    }
 
 }
